089a5cd54bf8a31709d67094ec6a459b7ebcd248
ebbabw <ebbabergholtz@gmail.com>
2020-03-06 23:41:07 +0100

    Did 1-4 (4 passed the first but not the rest

--- src/2-first-last.js
+++ src/2-first-last.js
@@ -1,3 +1,13 @@
+
+const msg = 'No items!'
+
 export const firstLast = (items) => {
-  return `First: ${items[0]}, Last: ${items[1]}`
-}
+    if(items.length === 0) {
+      return msg;
+    } else if (items.length === 1) {
+      return `Only item: ${items[0]}`
+    } else if (items.length > 1) {
+      return `First: ${items[0]}, Last: ${items[items.length - 1]}`
+    }
+} 
+
